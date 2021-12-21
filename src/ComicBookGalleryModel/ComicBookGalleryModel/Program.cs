5c7decc1a08afc695f5282bc62295499abc22110
Beatrice Bordignon <beatrice.bordignon@outlook.com>
2021-12-21 14:45:43 +0100

    Added an entity "ComicBook" and the "Context" and connected the Main to it

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new Models.ComicBook()
+                {
+                    SeriesTitle = "The amazing Spiderman",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
