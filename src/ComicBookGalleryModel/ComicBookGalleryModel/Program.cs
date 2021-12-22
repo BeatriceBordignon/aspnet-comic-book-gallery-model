dd0915561403020021f42e5f6c02d24c992f6925
Beatrice Bordignon <beatrice.bordignon@outlook.com>
2021-12-22 22:25:50 +0100

    Added a new entity class "Artist" and create a MANY-TO-MANY relationship with "ComicBook" entity.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -11,32 +11,78 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                var series = new Series()
+                var series1 = new Series()
                 {
                     Title = "The amazing Spiderman"
 
                 };
-                context.ComicBooks.Add(new Models.ComicBook()
+                var series2 = new Series()
                 {
-                    Series = series,
+                    Title = "The invincible Ironman"
+
+                };
+
+                var artist1 = new Artist()
+                {
+                    Name = "Stan Lee"
+                };
+                var artist2 = new Artist()
+                {
+                    Name = "Steve Dikto"
+                }; 
+                var artist3 = new Artist()
+                {
+                    Name = "Jack Kirby"
+                };
+
+                var comicBook1 = new ComicBook()
+                {
+                    Series = series1,
                     IssueNumber = 1,
                     PublishedOn = DateTime.Today
-                });
-                context.ComicBooks.Add(new Models.ComicBook()
+                };
+
+                comicBook1.Artist.Add(artist1);
+                comicBook1.Artist.Add(artist2);
+
+                var comicBook2 = new ComicBook()
                 {
-                    Series = series,
+                    Series = series1,
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                };
+
+                comicBook2.Artist.Add(artist1);
+                comicBook2.Artist.Add(artist2);
+
+                var comicBook3 = new ComicBook()
+                {
+                    Series = series2,
                     IssueNumber = 2,
                     PublishedOn = DateTime.Today
-                });
+                };
+
+                comicBook3.Artist.Add(artist1);
+                comicBook3.Artist.Add(artist3);
+
+                context.ComicBooks.Add(comicBook1);
+                context.ComicBooks.Add(comicBook2);
+                context.ComicBooks.Add(comicBook3);
+
                 context.SaveChanges();
 
                 var comicBooks = context.ComicBooks
                     .Include(cb => cb.Series)
+                    .Include (cb => cb.Artist)
                     .ToList();
 
                 foreach (var comicBook in comicBooks)
                 {
+                    var artistNames = comicBook.Artist.Select(a => a.Name).ToList();
+                    var artistDisplayText = string.Join(", ", artistNames);
+
                     Console.WriteLine(comicBook.DisplayText);
+                    Console.WriteLine(artistDisplayText);
                 }
 
                 Console.ReadLine();
