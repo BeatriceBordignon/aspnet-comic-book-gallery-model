90c0b188257936bc6d90c7e1fa2c4037f7fb6140
Beatrice Bordignon <beatrice.bordignon@outlook.com>
2021-12-22 09:25:40 +0100

    Added a new entity "Series" and created relationship MANY-TO-ONE with the "ComicBook" entity

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Series.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Series.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
-    public class ComicBook 
+    public class Series
     {
         public int Id { get; set; }
-        public Series Series { get; set; }
-        public int IssueNumber { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime PublishedOn { get; set; }
-        public decimal? AverageRating { get; set; }
+
+        public ICollection<ComicBook> ComicBooks { get; set; }
     }
 }
